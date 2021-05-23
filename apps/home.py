48ff6923f9f4bf0fa16d068bef061f9264e0a25c
chakradar <chakradar10@hotmail.com>
2021-05-23 07:36:21 +0530

    Add files via upload

---
+++ apps/home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def app():
+    st.title('Home')
+
+    st.write('This web provides you to enter a no-code environment of machine learning')
+
+    st.write('Just upload ur data by clicking on the top left side icon and you will asked to provide your data currently in just .csv format.')
+
+    st.write('Thanks for using the app and kindly provide the feedback on the github repo!')
